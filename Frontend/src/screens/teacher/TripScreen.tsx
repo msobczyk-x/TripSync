a0f8606cc0f3de2c1186f7e1455704e04b8e25f9
msobczyk-x <msobczyk55@wp.pl>
2023-05-14 23:06:42 +0200

    pierwsze pokazanie

--- Frontend/src/screens/teacher/TripScreen.tsx
+++ Frontend/src/screens/teacher/TripScreen.tsx
@@ -1,16 +1,40 @@
 
-import { Box, Heading, Text } from 'native-base'
+import { Box, Heading, Text, VStack, Flex, Button } from 'native-base'
 import React from 'react'
 import { useAuth } from '../../providers/AuthProvider'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 const TripScreen = () => {
   const {state} = useAuth();
   return (
-   <Box flex="1" safeAreaTop>
+   <Box flex="1" safeAreaTop bgColor={"white"}>
       {state.trip === null ? <Heading size={"sm"} >No current trip</Heading> :
-              <Box>
-              <Heading size={"sm"}  >{state.trip.trip_name} </Heading>
-              <Text fontSize={15} fontWeight={700} pb="2" >{state.trip.trip_description}</Text>
-              </Box>
+              <VStack space={10} p="8">
+              <Heading textAlign={"center"}  >{state.trip.trip_name} </Heading>
+              <Text fontSize={15} fontWeight={700}  >{state.trip.trip_description}</Text>
+
+              <Flex w={"100%"} alignItems={"center"} justifyContent={"space-between"} direction='row' bgColor={"blue.400"} p={10} rounded={"full"} >
+              <Text color={"white"}>{state.trip.start_location}</Text>
+          
+
+    <MaterialCommunityIcons name="navigation" size={24} color="white" style={{
+                transform: [{ rotate: `90deg` }]
+              }} />
+           
+              <Text color={"white"}>{state.trip.end_location}</Text>
+              </Flex>
+              <Flex direction='row' alignItems={"center"} justifyContent={"space-between"}>
+             <Heading>
+              Photos 
+             </Heading>
+             <Button>
+                Add
+              </Button>
+             </Flex>
+              <Flex>
+              <Text textAlign={"center"}>No photos</Text>
+              </Flex>
+
+              </VStack>
   
 }
 </Box>
